2b027c26759399e5a417e583316511a1b192417e
Yutaro M <yutaroapp@gmail.com>
2019-04-17 19:33:45 +0900

    0004-Fragmentによる画面遷移の実装。 ・Fragmentによる画面遷移処理が冗長のためリファクタリングしようとしたが失敗。一旦保留！

--- app/src/main/java/com/example/yutaroapp/spotapp/MainActivity.java
+++ app/src/main/java/com/example/yutaroapp/spotapp/MainActivity.java
@@ -6,11 +6,12 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -32,43 +33,48 @@ public class MainActivity extends AppCompatActivity {
     // 処理が冗長なので要検討
     ImageButton statusButton = (ImageButton)findViewById(R.id.footer_menu).
             findViewById(R.id.footer_menu_button_status).findViewById(R.id.footer_menu_button_img_button);
-    statusButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        StatusFragment statusFragment = new StatusFragment();
-        openFragment(statusFragment);
-      }
-    });
+    statusButton.setOnClickListener(this);
 
     ImageButton pointButton = (ImageButton)findViewById(R.id.footer_menu).
             findViewById(R.id.footer_menu_button_point).findViewById(R.id.footer_menu_button_img_button);
-    pointButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        PointFragment pointFragment = new PointFragment();
-        openFragment(pointFragment);
-      }
-    });
+    pointButton.setOnClickListener(this);
 
     ImageButton spoitButton = (ImageButton)findViewById(R.id.footer_menu).
             findViewById(R.id.footer_menu_button_spoit).findViewById(R.id.footer_menu_button_img_button);
-    spoitButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        SpoitFragment spoitFragment = new SpoitFragment();
-        openFragment(spoitFragment);
-      }
-    });
+    spoitButton.setOnClickListener(this);
 
     ImageButton settingButton = (ImageButton)findViewById(R.id.footer_menu).
             findViewById(R.id.footer_menu_button_setting).findViewById(R.id.footer_menu_button_img_button);
-    settingButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        SettingFragment settingFragment = new SettingFragment();
-        openFragment(settingFragment);
+    settingButton.setOnClickListener(this);
+  }
+
+  @Override
+  // 共通して、footer_menu_button_img_buttonのIDを送ってる。。。
+  // 考えられる対策として、共通化して居るID（footer_menu_button_img_button）を
+  // 上書いて固有のIDにする。（他にもっといい方法がありそう）
+  public void onClick(View v) {
+    if (v != null) {
+      Log.d("LOG: ", String.valueOf(v.getId()));
+      switch (v.getId()) {
+        case R.id.footer_menu_button_status:
+          StatusFragment statusFragment = new StatusFragment();
+          openFragment(statusFragment);
+          break;
+        case R.id.footer_menu_button_point:
+          PointFragment pointFragment = new PointFragment();
+          openFragment(pointFragment);
+          break;
+//        case R.id.footer_menu_button_img_button: 左のようにすれば引っかかる。
+        case R.id.footer_menu_button_spoit:
+          SpoitFragment spoitFragment = new SpoitFragment();
+          openFragment(spoitFragment);
+          break;
+        case R.id.footer_menu_button_setting:
+          SettingFragment settingFragment = new SettingFragment();
+          openFragment(settingFragment);
+          break;
       }
-    });
+    }
   }
 
   protected void openFragment(Fragment fragment) {
